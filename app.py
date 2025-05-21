8998fb48c12a20939042e3940844f65f87947ce0
Mat-Vieira <167985912+Mat-Vieira@users.noreply.github.com>
2025-05-21 08:04:57 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,36 +2,34 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-noticias = [
-    {
-        'id': 'deadisland3',
-        'titulo': 'Texto teste 1',
-        'descricao': 'Descrição da notícia 1',
-        'imagem': 'https://assets.xboxservices.com/assets/1f/c8/1fc8ce3b-60b6-422a-84d3-16858472ccde.jpg?n=745632_GLP-Page-Hero-1084_1920x1080.jpg',
-    },
-    {
-        'id': 'deadisland2',
-        'titulo': 'Texto teste 1',
-        'descricao': 'Descrição da notícia 1',
-        'imagem': 'https://assets.xboxservices.com/assets/1f/c8/1fc8ce3b-60b6-422a-84d3-16858472ccde.jpg?n=745632_GLP-Page-Hero-1084_1920x1080.jpg',
-    },
-    {
-        'id': 'vibrantvisuals',
-        'titulo': 'Texto teste 2',
-        'descricao': 'Descrição da notícia 2',
-        'imagem': 'https://pub-f354ec240bea480db7320bd0e29d972e.r2.dev/sites/8/2025/03/Minecraft_03_Vibrant_Visuals-dc564aedc997d04ebbf5.jpg',
-    },
-    {
-        'id': 'GTA6',
-        'titulo': 'Texto teste 3',
-        'descricao': 'Descrição da notícia 3',
-        'imagem': 'https://cdn.mos.cms.futurecdn.net/hBWx3UpU2Je2zJqrHLyUQW.jpg',
-    }
-]
-
 @app.route('/')
 def index():
     return render_template('index.html')
+
+noticias = [
+    {
+        'id': 'deadisland2',
+        'titulo': 'Texto teste',
+        'descricao': 'Texto teste',
+        'imagem': 'https://assets.xboxservices.com/assets/1f/c8/1fc8ce3b-60b6-422a-84d3-16858472ccde.jpg?n=745632_GLP-Page-Hero-1084_1920x1080.jpg',
+        'fonte': ''
+    },
+    {
+        'id': 'vibrantvisuals',
+        'titulo': 'Texto teste',
+        'descricao': 'Texto teste',
+        'imagem': 'https://pub-f354ec240bea480db7320bd0e29d972e.r2.dev/sites/8/2025/03/Minecraft_03_Vibrant_Visuals-dc564aedc997d04ebbf5.jpg',
+        'fonte': ''
+    },
+    {
+        'id': 'GTA6',
+        'titulo': 'Texto teste',
+        'descricao': 'Texto teste',
+        'imagem': 'https://cdn.mos.cms.futurecdn.net/hBWx3UpU2Je2zJqrHLyUQW.jpg',
+        'fonte': '',
+    }
+]
+
 
 @app.route('/acesso', methods=['POST'])
 def login():
@@ -51,15 +49,16 @@ def login_form():
 
 @app.route('/servicos')
 def servicos():
-    return render_template('servicos.html')
+    return render_template('servicos.html', noticias=noticias)
 
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
 
-@app.route('/chat.html')
-def chat():
-    return render_template('chat.html')
+@app.route('/chat/<id>')
+def chat(id):
+    postagem = next((n for n in noticias if n['id'] == id), None)
+    return render_template('chat.html', postagem=postagem)
 
 if __name__ == '__main__':
     app.run(debug=True)
