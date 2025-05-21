358fcd28f1e9026495c574747af53a791bb84d46
Forellii <2008.mv.mv@gmail.com>
2025-05-21 18:59:25 -0300

    Pagina para Chats

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+#@app.route('/')
+#def index():
+ #   return render_template('index.html')
 
 noticias = [
+    
     {
         'id': 'deadisland2',
-        'titulo': 'Texto teste',
+        'titulo': 'Dead Island 2 de graça na Epic Games Store',
         'descricao': 'Texto teste',
-        'imagem': 'https://assets.xboxservices.com/assets/1f/c8/1fc8ce3b-60b6-422a-84d3-16858472ccde.jpg?n=745632_GLP-Page-Hero-1084_1920x1080.jpg',
+        'imagem': 'https://www.adrenaline.com.br/wp-content/uploads/2023/04/dead-island-2-912x569.jpg',
         'fonte': ''
     },
     {
         'id': 'vibrantvisuals',
-        'titulo': 'Texto teste',
+        'titulo': 'Vibrant Visuals implementado no Minecraft via Experimento',
         'descricao': 'Texto teste',
         'imagem': 'https://pub-f354ec240bea480db7320bd0e29d972e.r2.dev/sites/8/2025/03/Minecraft_03_Vibrant_Visuals-dc564aedc997d04ebbf5.jpg',
         'fonte': ''
     },
     {
         'id': 'GTA6',
-        'titulo': 'Texto teste',
+        'titulo': 'GTA 6 pode ser adiado antes de 26 de Maio?',
         'descricao': 'Texto teste',
         'imagem': 'https://cdn.mos.cms.futurecdn.net/hBWx3UpU2Je2zJqrHLyUQW.jpg',
         'fonte': '',
@@ -47,7 +48,7 @@ def login():
 def login_form():
     return render_template('login.html')
 
-@app.route('/servicos')
+@app.route('/')
 def servicos():
     return render_template('servicos.html', noticias=noticias)
 
@@ -55,10 +56,12 @@ def servicos():
 def cadastro():
     return render_template('cadastro.html')
 
-@app.route('/chat/<id>')
-def chat(id):
-    postagem = next((n for n in noticias if n['id'] == id), None)
-    return render_template('chat.html', postagem=postagem)
+@app.route('/chat/<chat_id>')
+def chat(chat_id):
+    noticia = next((n for n in noticias if n['id'] == chat_id), None)
+    if not noticia:
+        return redirect(url_for('servicos'))
+    return render_template('chat.html', noticias=noticias, noticia=noticia)
 
 if __name__ == '__main__':
     app.run(debug=True)
