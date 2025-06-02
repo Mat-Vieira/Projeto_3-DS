d6802c0e42fc2107c4a1f3ca2035ab4f3c5614c7
mat-vieira <mateus.vieira22@escola.pr.gov.br>
2025-06-02 07:54:17 -0300

    update

--- app.py
+++ app.py
@@ -4,27 +4,34 @@ app = Flask(__name__)
 
 #@app.route('/')
 #def index():
- #   return render_template('index.html')
+#   return render_template('index.html')
 
 noticias = [
-    
+
+    {
+        'id': 'starfield',
+        'titulo': 'Starfield',
+        'descricao': 'Texto teste',
+        'imagem': 'https://www.adrenaline.com.br/wp-content/uploads/2023/08/starfield-912x569.jpg',
+        'fonte': ''
+    },
     {
         'id': 'deadisland2',
-        'titulo': 'Dead Island 2 de graça na Epic Games Store',
+        'titulo': 'Dead Island 2',
         'descricao': 'Texto teste',
         'imagem': 'https://www.adrenaline.com.br/wp-content/uploads/2023/04/dead-island-2-912x569.jpg',
         'fonte': ''
     },
     {
         'id': 'vibrantvisuals',
-        'titulo': 'Vibrant Visuals implementado no Minecraft via Experimento',
+        'titulo': 'Minecraft',
         'descricao': 'Texto teste',
         'imagem': 'https://pub-f354ec240bea480db7320bd0e29d972e.r2.dev/sites/8/2025/03/Minecraft_03_Vibrant_Visuals-dc564aedc997d04ebbf5.jpg',
         'fonte': ''
     },
     {
         'id': 'GTA6',
-        'titulo': 'GTA 6 pode ser adiado antes de 26 de Maio?',
+        'titulo': 'GTA 6',
         'descricao': 'Texto teste',
         'imagem': 'https://cdn.mos.cms.futurecdn.net/hBWx3UpU2Je2zJqrHLyUQW.jpg',
         'fonte': '',
