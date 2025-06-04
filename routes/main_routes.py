dc649e2bc3f2a26617331e47faea8e3546388f77
22022008m4t <mateus.vieira22@escola.pr.gov.br>
2025-06-04 08:11:25 -0300

    update

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -7,30 +7,9 @@ noticias = [
     {
         'id': 'starfield',
         'titulo': 'Starfield',
-        'descricao': 'Texto teste',
+        'descricao': 'Texto teste Texto teste Texto teste Texto teste Texto teste',
         'imagem': 'https://www.adrenaline.com.br/wp-content/uploads/2023/08/starfield-912x569.jpg',
         'fonte': ''
-    },
-    {
-        'id': 'deadisland2',
-        'titulo': 'Dead Island 2',
-        'descricao': 'Texto teste',
-        'imagem': 'https://www.adrenaline.com.br/wp-content/uploads/2023/04/dead-island-2-912x569.jpg',
-        'fonte': ''
-    },
-    {
-        'id': 'vibrantvisuals',
-        'titulo': 'Minecraft',
-        'descricao': 'Texto teste',
-        'imagem': 'https://pub-f354ec240bea480db7320bd0e29d972e.r2.dev/sites/8/2025/03/Minecraft_03_Vibrant_Visuals-dc564aedc997d04ebbf5.jpg',
-        'fonte': ''
-    },
-    {
-        'id': 'GTA6',
-        'titulo': 'GTA 6',
-        'descricao': 'Texto teste',
-        'imagem': 'https://cdn.mos.cms.futurecdn.net/hBWx3UpU2Je2zJqrHLyUQW.jpg',
-        'fonte': '',
     }
 ]
 
